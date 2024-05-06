8936b2e9fb64661b8f259e516da4fe91e1844f85
crystale14 <crystale.trg@outlook.com>
2024-05-06 16:34:14 +0200

    first fxml draft

--- CY-Books/src/main/java/com/example/cybooks/CYBooks.java
+++ CY-Books/src/main/java/com/example/cybooks/CYBooks.java
@@ -3,6 +3,7 @@ package com.example.cybooks;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,8 +12,9 @@ public class CYBooks extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(CYBooks.class.getResource("CYBooks.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("CY-Books");
         stage.setScene(scene);
         stage.show();
     }
